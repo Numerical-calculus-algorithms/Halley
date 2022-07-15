ab36acc53b787091d6163249e6b56f59726108c0
Vito Proscia <vitoproscia.1704@gmail.com>
2022-07-15 12:47:39 +0200

    Aggiunto calcolo residuo

--- Halley.m
+++ Halley.m
@@ -8,13 +8,13 @@ for nit = 1:maxIter
     fx = f(x0); % Function evaluation
     df1x = df1(x0); % First derivate evaluation
     df2x = df2(x0); % Second derivate evaluation
-    res(end + 1) = fx; 
+    res(end + 1, 1) = fx; 
     x = x0 - ( (2*fx*df1x)/(2*(df1x^2) - df2x)); % Iteration formula
     err = abs(x-x0)/abs(x); % Relative error calculation
     if err < tol
         return
     end
-    fprintf('%i \t%1.10f \t  %.2e\n', nit, x, err)
+    fprintf('%i \t%1.10f \t  %.2e \t  %.2e\n', nit, x, err, res(end))
     x0 = x; 
 
 end
